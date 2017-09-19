29bd4428589c22e484424b9ee55feeeb3f16ed5c
Tanji Saraf-Chávez <tanjischavez@gmail.com>
2017-09-18 21:59:32 -0700

    Add files via upload

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,23 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    b = a
+    x = 0
+    for strings in b
+    	b[x] = b[x].to_i
+    	b[x] += 2
+    	x += 1
+    end
+    b.delete_if {|b| b % 2 != 0 } 
+    b.uniq
+    b.delete_if {|b| b > 10}
+    c = 0
+    xx = 0
+    for number in b
+    	c += b[xx]
+    	xx += 1
+    end
+    return c
+
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,26 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  z = a + b
+  return z ** 2
+
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  b = a.sort
+  x = 0
+  for number in b
+  	b[x] = number += 1
+  	x += 1
+  end
+  return b
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  # return first_name + " " + last_name
+  return "#{first_name} #{last_name}"
 
 end
 
